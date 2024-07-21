98bb90a72883eb76b52421c1a1bd969d1de4daa5
malekcoders <149315149+malekcoders@users.noreply.github.com>
2024-07-21 10:21:35 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
 public class Main {
 
-    public static void main(String[] args) {
+    
+public static void main(String[] args) {
+    WashQueue queue = new WashQueue();
+    queue.addCar(new Car("Sedan", "Red", "1234"));
+    queue.addCar(new Car("SUV", "Black", "5678"));
+    queue.addCar(new Car("Truck", "White", "91011"));
+    queue.addCar(new Car("Sedan", "Blue", "1213"));
 
-        System.out.println("Working...");
-    }
+    queue.printFirstCar();
+    queue.printTotalCars();
+    
+    queue.washCar();
+
+    queue.printFirstCar();
+    queue.printTotalCars();
+}
+
     
 }
